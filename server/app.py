be3d12ecc506adf69de8a1178800f0003d614df0
Clement <kwasiclement764@gmail.com>
2025-03-26 08:49:21 +0000

    Update options app.py

--- server/app.py
+++ server/app.py
@@ -5,6 +5,13 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app, origins=["http://localhost:3000"])
 
+@app.after_request
+def after_request(response):
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"]
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+    return response
+
 def get_video_info(video_url):
     ydl_opts = {
         'quiet': True,
